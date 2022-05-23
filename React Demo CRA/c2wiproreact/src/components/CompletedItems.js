79f4c9203236a38ca8af33226255a25c32cfcdcf
shruthi093 <jayasruthi.l@gmail.com>
2022-05-23 19:50:09 +0530

    todo app

---
+++ React Demo CRA/c2wiproreact/src/components/CompletedItems.js
@@ -0,0 +1,21 @@
+ const CompeletedItems=(props)=>{
+    console.log("this is props",props.deleteFun)
+    const styleobj={
+        backgroundColor:"aqua"
+    }
+    return(
+        <div style={styleobj}>
+            <h3>Completed Tasks list</h3>
+            {
+                props.tododata.map((item)=>{
+                    return item.completed&&<div>
+                        <h3>{item.title}</h3>
+                        <p>{item.text}</p>
+                        <button onClick={()=>props.deleteFun(item.id)}>delete</button>
+                        </div>
+                })
+            }
+        </div>
+    )
+}
+export default CompeletedItems
--- React Demo CRA/c2wiproreact/src/components/TodoComponent.js
+++ React Demo CRA/c2wiproreact/src/components/TodoComponent.js
@@ -1,44 +1,69 @@
 import React from "react"
+import CompeletedItems from "./CompletedItems"
 export default class TodoComponent extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            todoData:[
-                {
-                    title:"buy eggs",
-                    text:"buy 12 eggs"
-                }
-            ],
+            todoData:[],
             todoTitle:"",
-            todoText:""
+            todoText:"",
+            todoid:0
         }
     }
     addItem=()=>{
+        this.setState({todoid:this.state.todoid+1})
         this.setState({todoData:[...this.state.todoData,{
+            id:this.state.todoid,
             title:this.state.todoTitle,
-            text:"build the app by this weekend"
+            text:this.state.todoText,
+            completed:false
         }]})
     }
     handleTitle=(ev)=>{
-        console.log(ev.target.value)
+      
         this.setState({todoTitle:ev.target.value})
     }
+    handleText=(ev)=>{
+       
+        this.setState({todoText:ev.target.value})
+    }
+    changeCompletion=(itemid)=>{
+    const temp=[...this.state.todoData]
+     const obj= temp.find((item)=>item.id===itemid)
+     obj.completed=!obj.completed
+     this.setState({todoData:temp})
+    }
+    deleteTask=(itemid)=>{
+        const temp=[...this.state.todoData]
+        const result=temp.filter((item)=>item.id!==itemid)
+        this.setState({todoData:result})
+    }
     render(){
-      
+        const styleobj={
+            backgroundColor:"grey"
+        }
+      console.log(this.state.todoData)
         return(
-            
             <div>
                Title: <input type="text" onChange={(e)=>this.handleTitle(e)}></input>
-               Enter Description: <input type="text"></input>
+               Enter Description: <input type="text" onChange={(e)=>this.handleText(e)}></input>
                 <button onClick={this.addItem}>addItem</button>
+                <div style={styleobj}>
+                <h3>Pending Items</h3>
                 {
                     this.state.todoData.map((item)=>(
+                        item.completed?<></>:
                         <div>
                             <h2>{item.title}</h2>
                             <p>{item.text}</p>
+                         <button onClick={()=>this.changeCompletion(item.id)}>done</button>
+                         <button onClick={()=>this.deleteTask(item.id)}>delete</button>
                         </div>
                     ))
                 }
+                </div>
+                <CompeletedItems tododata={this.state.todoData} deleteFun={this.deleteTask}></CompeletedItems>
+
             </div>
         )
     }
