4e2a043b67564835ae9d69d9ce4ca5ad322b6e3c
shruthi093 <jayasruthi.l@gmail.com>
2022-05-25 18:53:44 +0530

    usestate and TSX

---
+++ React Demo CRA/c2wiproreact/src/components/FunctionalComponent.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+
+const FunctionalComponent=()=>{
+    const [id,setId]=useState(0)
+    const [todoData,setTodoData]=useState([])
+    const [todoTitle,setTitle]=useState("")
+    
+    const addItem=()=>{
+        setId(id+1)
+       setTodoData([...todoData,{id:id,todoTitle:todoTitle}])
+
+    }
+    return(
+        <div>
+            {count}
+            <button onClick={increment}>+</button>
+        </div>
+    )
+}
